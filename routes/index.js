f46b4f74367aa8b73d037ef410d3ef176d83c748
Aaron C. de Bruyn <aaron@heyaaron.com>
2024-10-31 07:18:58 -0700

    Updates

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var context = {
-  title: 'CTRL ALT IT : Home',
-  company_name: 'CTRL ALT IT',
-  company_slogan: 'Reboot your network',
+  title: 'NogCorp',
+  company_name: 'NogCorp',
+  company_slogan: 'Nog for Everyone!',
   copyright_year: new Date().getFullYear()
 };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', context);
-});
-
-router.get('/privacy/', function(req, res, next) {
-  res.render('privacy', context);
-});
-
-router.get('/terms-of-service/', function(req, res, next) {
-  res.render('tos', context);
-});
-
-router.get('/story/cryptolocker/', function(req, res, next) {
-  res.render('story_cryptolocker', context);
 });
 
 module.exports = router;
